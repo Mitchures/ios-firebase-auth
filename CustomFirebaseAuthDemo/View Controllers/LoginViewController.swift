663ce59b413867fb815cacf699aa15a850a0da52
Mitchell Hollander <MitchellSHollander@gmail.com>
2020-05-01 14:02:07 -0500

    added video and login modifications

--- CustomFirebaseAuthDemo/View Controllers/LoginViewController.swift
+++ CustomFirebaseAuthDemo/View Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -29,14 +30,52 @@ class LoginViewController: UIViewController {
         errorLabel.alpha = 0
     }
 
+    func validateFields() -> String? {
+        // check if all fields are filled
+        if emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all fields."
+        }
+        
+        return nil
+    }
+    
+    func showError(_ message:String) {
+        
+        errorLabel.text = message
+        errorLabel.alpha = 1
+        
+    }
     
     
     @IBAction func loginTapped(_ sender: Any) {
         
         // validate text fields
+        let error = validateFields()
         
-        // sign in user
-        
+        if error != nil {
+            showError(error!)
+        }
+        else {
+            // sign in user
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            Auth.auth().signIn(withEmail: email, password: password) { (result, err) in
+                if err != nil {
+                    self.showError(err!.localizedDescription)
+                }
+                else {
+                    
+                    let homeViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+                    
+                    self.view.window?.rootViewController = homeViewController
+                    self.view.window?.makeKeyAndVisible()
+                    
+                }
+            }
+        }
     }
     
 }
--- CustomFirebaseAuthDemo/View Controllers/ViewController.swift
+++ CustomFirebaseAuthDemo/View Controllers/ViewController.swift
@@ -7,15 +7,56 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    var videoPlayer:AVPlayer?
+    var videoPlayerLayer:AVPlayerLayer?
+    
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setUpVideo()
+    }
+    
+    func setUpVideo() {
+        
+        // Get the video source path
+        let bundlePath = Bundle.main.path(forResource: "tokyoVideo", ofType: "mov")
+        
+        guard  bundlePath != nil else {
+            return
+        }
+        
+        // Create a URL from path
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        // Create the video player item
+        let item = AVPlayerItem(url: url)
+        
+        // Create the player
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        // Create the layer
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        
+        // Adjust the size and frame
+        videoPlayerLayer?.frame = CGRect(x:
+            -self.view.frame.size.width * 1.5, y: 0, width:
+            self.view.frame.size.width * 4, height:
+            self.view.frame.size.height)
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        
+        // Display
+        videoPlayer?.playImmediately(atRate: 1)
+        
     }
 
     @IBAction func signUpTapped(_ sender: Any) {
